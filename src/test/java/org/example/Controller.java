d8cfead77c9d96371751f589848e0a3e8664a466
chaos <958273999@qq.com>
2024-12-05 16:30:55 +0800

    1

--- src/test/java/org/example/Controller.java
+++ src/test/java/org/example/Controller.java
@@ -5,7 +5,7 @@ import util.ShellRunner;
 
 public class Controller {
     private static final String TEST_TASK_FILE = "./src/test/resources/testTask.txt";
-    private static final String MVN_RUN_TEST = "../.github/scripts/test/test_union.sh";
+    private static final String MVN_RUN_TEST = "./.github/scripts/test/test_union.sh";
     @Test
     public void testUnion() throws Exception {
         ShellRunner shellRunner = new ShellRunner();
