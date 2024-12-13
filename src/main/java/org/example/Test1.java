9ec2821f8c71b834dddd71483d525f257554414d
chaos <958273999@qq.com>
2024-12-13 22:35:25 +0800

    1

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Test1 {
 
     private int a=0;
@@ -21,11 +25,24 @@ public class Test1 {
         t.setA(100);
     }
 
-    public static void main(String[] args) {
-        Test1 t=null;
 
-        test(t);
-        System.out.println(t.getA());
+    public static void main(String[] args) {
+//        Test1 t=null;
+//
+//        test(t);
+//        System.out.println(t.getA());
+
+        List<Integer> list= Arrays.asList(1,2,3);
+        List<String> list2= Arrays.asList("a","b","c");
+        List list1 = list;
+        for (int i = 0; i < list1.size(); i++){
+            System.out.println(list1.get(i));
+        }
+        list1 = list2;
+        for (int i = 0; i < list1.size(); i++){
+            System.out.println(list1.get(i));
+        }
+
     }
 
 
